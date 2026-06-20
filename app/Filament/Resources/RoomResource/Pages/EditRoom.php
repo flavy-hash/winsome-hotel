1fd98bbcec02e9511daab19151e0c45f8dd455f4
flavy-hash <flavianmichael663@gmail.com>
2026-06-20 11:35:46 +0300

    resource to edit room via filament

--- app/Filament/Resources/RoomResource/Pages/EditRoom.php
+++ app/Filament/Resources/RoomResource/Pages/EditRoom.php
@@ -16,4 +16,17 @@ class EditRoom extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (empty($data['image_path'])) {
+            $data['image_path'] = $this->record->image_path;
+        }
+
+        if (empty($data['gallery'])) {
+            $data['gallery'] = $this->record->gallery;
+        }
+
+        return $data;
+    }
 }
