dee56e68ecd85509b6099d13d826aa574ea1c8b9
flavy-hash <flavianmichael663@gmail.com>
2026-06-20 11:50:08 +0300

    for SEO optimazation

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,40 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+        <lastmod>{{ now()->toDateString() }}</lastmod>
+    </url>
+
+    <url>
+        <loc>{{ route('rooms.index') }}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+        <lastmod>{{ now()->toDateString() }}</lastmod>
+    </url>
+
+    <url>
+        <loc>{{ route('location') }}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+    <url>
+        <loc>{{ route('reviews.index') }}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+        <lastmod>{{ now()->toDateString() }}</lastmod>
+    </url>
+
+    @foreach($rooms as $room)
+    <url>
+        <loc>{{ route('rooms.show', $room) }}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+        <lastmod>{{ $room->updated_at->toDateString() }}</lastmod>
+    </url>
+    @endforeach
+
+</urlset>
